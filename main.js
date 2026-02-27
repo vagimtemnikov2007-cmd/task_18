e2c7c9528695befdab877cc4db09c9277b62a0f5
vagimtemnikov2007-cmd <vagim.temnikov.2007@gmail.com>
2026-02-27 08:44:26 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const skills = ["HTML", "CSS", "JavaScript", "GitHub", "LocalStorage"];
     const list = document.getElementById('skills-list');
-
+    console.log("Переменная list сейчас:", list);
     if (list) {
         renderSkills(skills, list);
     }
@@ -14,5 +14,7 @@ function renderSkills(data, container) {
         const li = document.createElement('li');
         li.textContent = skill;
         container.appendChild(li);
+        console.log("Итерация цикла для элемента: " + skill);
+
     });
 }
